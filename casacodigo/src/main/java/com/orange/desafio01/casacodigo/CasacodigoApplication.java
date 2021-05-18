23ea6ba7b0b143028e12427abe1b86187bef9c6d
leonardoinatel <leonardodamasceno@gec.inatel.br>
2021-05-18 14:40:39 -0300

    Git to chance branch

--- casacodigo/src/main/java/com/orange/desafio01/casacodigo/CasacodigoApplication.java
+++ casacodigo/src/main/java/com/orange/desafio01/casacodigo/CasacodigoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CasacodigoApplication {
-
+	// java project commit to chance branch
 	public static void main(String[] args) {
 		SpringApplication.run(CasacodigoApplication.class, args);
 	}

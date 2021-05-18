6852331c99313f218a62a6aea1f90e3726f97713
leonardoinatel <leonardodamasceno@gec.inatel.br>
2021-05-18 14:34:25 -0300

    solving bug of datasourceAutoConfigure.class

--- casacodigo/src/main/java/com/orange/desafio01/casacodigo/CasacodigoApplication.java
+++ casacodigo/src/main/java/com/orange/desafio01/casacodigo/CasacodigoApplication.java
@@ -2,12 +2,12 @@ package com.orange.desafio01.casacodigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CasacodigoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CasacodigoApplication.class, args);
 	}
-
 }

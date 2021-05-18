45cd885d0b10b770cae6162f2260be7cf2a70b5c
leonardoinatel <leonardodamasceno@gec.inatel.br>
2021-05-18 14:04:13 -0300

    Desafio Casa do Codigo

---
+++ casacodigo/src/main/java/com/orange/desafio01/casacodigo/CasacodigoApplication.java
@@ -0,0 +1,13 @@
+package com.orange.desafio01.casacodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasacodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasacodigoApplication.class, args);
+	}
+
+}
---
+++ casacodigo/src/test/java/com/orange/desafio01/casacodigo/CasacodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.desafio01.casacodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasacodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

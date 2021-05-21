c655de053f6c1855f9c8554b87e89e56c188b599
jaine <doerzbacherjaine@gmail.com>
2021-05-21 11:43:09 -0300

    Criação da funcnalidade cadastro de autor

---
+++ src/main/java/casadocodigo/casadocodigo/entities/Autor.java
@@ -0,0 +1,47 @@
+package casadocodigo.casadocodigo.entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_autor")
+public class Autor{
+
+
+    @NotNull @NotBlank
+    @Column(name = "NOME_AUTOR")
+    private String nomeAutor;
+    @NotNull @NotBlank
+    @Column(name = "EMAIL_AUTOR")
+    private String emailAutor;
+    @NotNull @NotBlank
+    @Column(name = "DESC_AUTOR", length = 400)
+    private String descAutor;
+    @Column(name = "LOCAL")
+    private LocalDateTime local;
+
+
+    public Autor(String nomeAutor, String emailAutor, String descAutor, LocalDateTime local) {
+
+        this.nomeAutor = nomeAutor;
+        this.emailAutor = emailAutor;
+        this.descAutor = descAutor;
+        this.local = local;
+    }
+
+
+    public Autor(String nomeAutor, String emailAutor, String descAutor) {
+        this.nomeAutor = nomeAutor;
+        this.emailAutor = emailAutor;
+        this.descAutor = descAutor;
+    }
+
+    public Autor() {
+    }
+}
---
+++ src/main/java/casadocodigo/casadocodigo/gateway/controller/AutorController.java
@@ -0,0 +1,30 @@
+package casadocodigo.casadocodigo.gateway.controller;
+
+import casadocodigo.casadocodigo.dtos.AutorDTO;
+import casadocodigo.casadocodigo.entities.Autor;
+import casadocodigo.casadocodigo.gateway.repositories.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/autor")
+public class AutorController{
+
+    private AutorRepository autorRepository;
+    public AutorController(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
+
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void cadastraAutor(@RequestBody @Valid AutorDTO request){
+
+        Autor autor = request.toModel();
+        autorRepository.save(autor);
+    }
+}
